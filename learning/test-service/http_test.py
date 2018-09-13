2687c9b14c7fac08804f1659f16ea5171dac0d5c
Mariam <mariam.shekoyan@gmail.com>
2018-09-13 22:23:48 +0400

    adding new test service

---
+++ learning/test-service/http_test.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--host", default="127.0.0.1")
+parser.add_argument("--port", default="94000", type=int)
+parser.add_argument("--debug", default="0", type=int)
+
+
+class Service:
+
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.args = parser.parse_args()
+
+        self.app.add_url_rule('/echo', 'echo', self.print_ext, methods=['POST'])
+
+    @staticmethod
+    def print_ext():
+        print request.json
+        return jsonify({'status': 'success', 'data': request.json})
+
+    def run(self):
+        try:
+            print self.args.port
+            self.app.run(host=self.args.host, port=self.args.port, debug=self.args.debug != 0)
+        except KeyboardInterrupt:
+            exit(1)
+
+
+serviceObj = Service()
+serviceObj.run()
